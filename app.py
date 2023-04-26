ca82b29dc42c854dfaf8ea94581eb942a0d288bf
William Boyles <williamboyles22900@gmail.com>
2023-04-25 22:53:15 -0700

    Use functools builtin cache

--- app.py
+++ app.py
@@ -2,30 +2,27 @@ from flask import Flask, render_template, Response
 from PIL import Image
 import os
 from io import BytesIO
+from functools import cache
 
 # TODO: How to handle multiple requests in parallel using async/await?
 app = Flask(__name__)
 
 # Define a cache to store the resized images
-cache: dict[str, BytesIO] = {}
 
-
+# TODO: How could we implement caching for clients, sending a 304 if the client already requested an image? This already heppens for /static/images
 @app.route("/resized/<filename>")
-def resized_image(filename, width=200):
+@cache
+def resized_image(filename, width=300):
     # Serve a resized image from the cache
     images_folder = os.path.join(app.static_folder, "images")
     filepath = os.path.join(images_folder, filename)
 
-    if filepath not in cache:
-        buffer = BytesIO()
-        with Image.open(filepath) as img:
-            img.thumbnail((width, width))
+    buffer = BytesIO()
+    with Image.open(filepath) as img:
+        img.thumbnail((width, width))
+        img.save(buffer, "JPEG")
 
-            img.save(buffer, "JPEG")
-
-        cache[filepath] = buffer
-
-    return Response(cache[filepath].getvalue(), mimetype='image/jpeg')
+    return Response(buffer.getvalue(), mimetype='image/jpeg')
 
 
 @app.route("/")
