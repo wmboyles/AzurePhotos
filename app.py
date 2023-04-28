a3f9786037aff22cb34e2c30c01076adfd165af0
William Boyles <williamboyles22900@gmail.com>
2023-04-27 22:20:53 -0700

    Add backend method for deleting

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ from werkzeug.utils import secure_filename
 
 # TODO: How to handle multiple requests in parallel using async/await?
 app = Flask(__name__)
-images_folder = os.path.join(app.static_folder, "images") # type: ignore
+images_folder = os.path.join(app.static_folder, "images")  # type: ignore
+delete_folder = os.path.join(app.static_folder, "deleted")  # type: ignore
+
 
 # TODO: How could we implement caching for clients, sending a 304 if the client already requested an image? This already heppens for /static/images
 @app.route("/resized/<filename>")
@@ -16,10 +18,10 @@ images_folder = os.path.join(app.static_folder, "images") # type: ignore
 def resized_image(filename: str):
     # Serve a resized image from the cache
     filepath = os.path.join(images_folder, filename)
-    
-    extension = filename.split('.')[-1]
-    if extension == 'jpg':
-        extension = 'jpeg'
+
+    extension = filename.split(".")[-1]
+    if extension == "jpg":
+        extension = "jpeg"
 
     buffer = BytesIO()
     with Image.open(filepath) as img:
@@ -31,16 +33,29 @@ def resized_image(filename: str):
 
 @app.route("/upload", methods=["POST"])
 def upload():
-    files = request.files.getlist('upload')
+    files = request.files.getlist("upload")
     for file in files:
         print(file)
 
         # TODO: Add image alphabetially earlier than all other images to it appears at the top
         save_filename = secure_filename(str(file.filename))
         file.save(os.path.join(images_folder, save_filename))
-    
+
     return redirect("/")
     # return Response(status=200)
+
+
+@app.route("/delete/<filename>", methods=["DELETE"])
+def delete(filename: str):
+    source = os.path.join(images_folder, filename)
+
+    if not os.path.exists(source):
+        return Response(status=404)
+
+    dest = os.path.join(delete_folder, filename)
+    os.replace(source, dest)
+
+    return Response(status=204)
 
 
 @app.route("/")
