857578b14313e5939ad38f15d9ec9cde60485cd9
William Boyles <williamboyles22900@gmail.com>
2023-04-29 00:32:02 -0700

    Add basic container copy function
    This demonstrates most of the things, except the photo resizing itself,
    for dealing with blobs in an Azure function.
    
    TODO: Instead of requesting the blob content by creating a source blob
    client, could we instead get the content directly from the input stream?

--- ImageResizer/function_app.py
+++ ImageResizer/function_app.py
@@ -1,9 +1,22 @@
 import azure.functions as func
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobClient
+import logging
 
 app = func.FunctionApp()
 
 
-@app.function_name(name="HttpTrigger1")
-@app.route(route="hello")  # HTTP Trigger
-def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("HttpTrigger1 function processed a request!!!")
+@app.function_name(name="Resizer")
+@app.blob_trigger(arg_name="myblob", path="photos", connection="PhotosConnection")
+def test_function(myblob: func.InputStream):
+    logging.info(f"Savan Says: {myblob.name=}")
+    logging.info(f"Savan Says: {myblob.uri=}")
+    name = str(myblob.name).split("/")[-1]
+    logging.info(f"Savan Says: {name=}")
+    account_url = "https://wboylesbackups.blob.core.windows.net"
+    credential = DefaultAzureCredential()
+
+    src_client = BlobClient(account_url, "photos", name, credential=credential)
+    des_client = BlobClient(account_url, "videos", name, credential=credential)
+
+    des_client.start_copy_from_url(src_client.url)
