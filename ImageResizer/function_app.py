210b928aee1e4a51b8b29418377c72ee01787e66
William Boyles <williamboyles22900@gmail.com>
2023-04-30 00:49:18 -0700

    Read from input stream instead of creating src client

--- ImageResizer/function_app.py
+++ ImageResizer/function_app.py
@@ -5,18 +5,28 @@ import logging
 
 app = func.FunctionApp()
 
+# func azure functionapp publish wboyles-resizer
 
 @app.function_name(name="Resizer")
 @app.blob_trigger(arg_name="myblob", path="photos", connection="PhotosConnection")
 def test_function(myblob: func.InputStream):
-    logging.info(f"Savan Says: {myblob.name=}")
-    logging.info(f"Savan Says: {myblob.uri=}")
+    logging.info(f"{myblob.name=}")
+    logging.info(f"{myblob.uri=}")
+    logging.info(f"Reading {myblob.length=} bytes")
+
+    blob_bytes = myblob.read()
+    logging.info(f"Found {len(blob_bytes)=} bytes")
+    
+
     name = str(myblob.name).split("/")[-1]
-    logging.info(f"Savan Says: {name=}")
+    logging.info(f"{name=}")
+
+    # # TODO: Either get this url from the input or from config
     account_url = "https://wboylesbackups.blob.core.windows.net"
     credential = DefaultAzureCredential()
+    logging.info("Created credential")
 
-    src_client = BlobClient(account_url, "photos", name, credential=credential)
+    # src_client = BlobClient(account_url, "photos", name, credential=credential)
     des_client = BlobClient(account_url, "videos", name, credential=credential)
 
-    des_client.start_copy_from_url(src_client.url)
+    des_client.upload_blob(blob_bytes)
