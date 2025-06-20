ee1b4d9c5ed5ebc3f6e9b003034d4d9ea29aa741
William Boyles <wmboyles@users.noreply.github.com>
2025-06-19 21:42:16 -0700

    Add storage health check

--- azurephotos/src/api/health.py
+++ azurephotos/src/api/health.py
@@ -4,7 +4,9 @@ API endpoints for managing health
 :author: William Boyles
 """
 
-from flask import Blueprint, Response
+from flask import Blueprint, Response, current_app
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, LinearRetry
 
 api_health_controller = Blueprint(
     "api_health_controller",
@@ -18,8 +20,16 @@ api_health_controller = Blueprint(
 @api_health_controller.route("/", methods=["GET"])
 def health() -> Response:
     """
-    If the app is running fine, respond with HTTP 200.
+    If the app is running fine and can talk to storage, respond with HTTP 200.
     """
-    
-    # TODO: check access to storage
-    return Response(status=200)
+
+    credential: DefaultAzureCredential = current_app.config["credential"]
+    account_name = current_app.config["account_name"]
+    blob_account_url: str = f"https://{account_name}.blob.core.windows.net"
+
+    try:
+        with BlobServiceClient(blob_account_url, credential, retry_policy=LinearRetry(retry_total=0)) as blob_client:
+            _ = blob_client.get_account_information()
+        return Response("ok", status=200, content_type="text/plain")
+    except Exception as e:
+        return Response(str(e), status=503, content_type="text/plain")
