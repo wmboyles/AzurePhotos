81e2e24e263b3c055a502958d79555acdde5a0df
William Boyles <wmboyles@users.noreply.github.com>
2026-03-14 14:26:31 -0700

    Correct path

--- azurephotos/startup.sh
+++ azurephotos/startup.sh
@@ -4,8 +4,8 @@ set -e
 echo "----- Custom startup: ensuring ffmpeg is installed -----"
 
 if ! command -v ffmpeg >/dev/null 2>&1; then
-    chmod +x $APP_PATH/bin/ffmpeg
-    chmod +x $APP_PATH/bin/ffprobe
+    chmod +x $APP_PATH/bin/ffmpeg-7.0.2-amd64-static/ffmpeg
+    chmod +x $APP_PATH/bin/ffmpeg-7.0.2-amd64-static/ffprobe
     export PATH="$APP_PATH/bin:$PATH"
 else
     echo "ffmpeg already installed."
