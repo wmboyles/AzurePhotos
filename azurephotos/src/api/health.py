bbacf69f2dfaba5fb36c0b3f62f11e03869133e7
William Boyles <wmboyles@users.noreply.github.com>
2026-05-09 18:32:18 -0700

    Container/table level health check

--- azurephotos/src/api/health.py
+++ azurephotos/src/api/health.py
@@ -5,7 +5,8 @@ API endpoints for managing health
 """
 
 from flask import Blueprint, Response, current_app
-from azure.storage.blob import BlobServiceClient
+from azure.data.tables import TableClient
+from azure.storage.blob import ContainerClient
 
 api_health_controller = Blueprint(
     "api_health_controller",
@@ -22,10 +23,21 @@ def health() -> Response:
     If the app is running fine and can talk to storage, respond with HTTP 200.
     """
 
-    blob_service_client: BlobServiceClient = current_app.config["blob_service_client"]
+    container_clients: list[ContainerClient] = [current_app.config[k] for k in [
+        "photos_container_client",
+        "videos_container_client",
+        "thumbnails_container_client"
+    ]]
+    table_clients: list[TableClient] = [current_app.config[k] for k in [
+        "albums_table_client"
+    ]]
 
     try:
-        _ = blob_service_client.get_account_information()
+        for container_client in container_clients:
+            _ = container_client.get_container_properties(timeout=5)
+        for table_client in table_clients:
+            _ = next(table_client.list_entities(results_per_page=1), None)
+        
         return Response("ok", status=200, content_type="text/plain")
     except Exception as e:
         return Response(str(e), status=503, content_type="text/plain")
